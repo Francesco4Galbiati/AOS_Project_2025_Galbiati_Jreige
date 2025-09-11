447d82be01b040687d13a00ab750c30bc80bbd1e
Francesco Galbiati <126344522+Francesco4Galbiati@users.noreply.github.com>
2025-09-11 09:55:05 +0200

    Update compile_cuda.sh

--- compile_cuda.sh
+++ compile_cuda.sh
@@ -2,7 +2,7 @@
 
 exe() { echo -e "\t\$ $@" ; "$@" ; }
 
-inputfile="cuspis_lava.cu"
+inputfile="cuspis_lava_kbtr.cu"
 libcuda="/usr/local/cuda/lib64/"
 
 exe nvcc -c $inputfile -o tmp.o -I./cuspis -g -G
